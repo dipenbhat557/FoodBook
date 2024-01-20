0f88ff0a016a58c7e99c9d7186e64bf92c24238c
dipenbhat557 <dipenbhat557@gmail.com>
2024-01-20 19:45:51 +0530

    :tada: initial comit

---
+++ backend/src/main/java/com/foodbook/FoodbookApplication.java
@@ -0,0 +1,13 @@
+package com.foodbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodbookApplication.class, args);
+	}
+
+}
